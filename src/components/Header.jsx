cc3145ca7ff64b5f78b9f76b585d4a701d8874f2
sebastian <sebastianseban0105@gmail.com>
2025-06-04 07:55:44 +0530

    feat(Header): implement search suggestions with debouncing and caching via Redux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,46 @@
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoSearch } from "react-icons/io5";
 import { HiUserCircle } from "react-icons/hi2";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../store/appSlice";
+import { useEffect, useState } from "react";
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
+import { cacheResults } from "../store/searchSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  const searchCache = useSelector((store) => store.search);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        getSearchSugsestions();
+      }
+    }, 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSugsestions = async () => {
+    const data = await fetch(`${YOUTUBE_SEARCH_API}${searchQuery}`);
+    const json = await data.json();
+    setSuggestions(json[1]);
+    dispatch(cacheResults({
+      [searchQuery] : json[1]
+    }))
+  };
 
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
   };
+
   return (
     <div className="grid grid-cols-12 shadow-md p-3 px-7 items-center">
       <div className="flex items-center cursor-pointer col-span-2">
@@ -25,15 +56,35 @@ const Header = () => {
       </div>
 
       <div className="col-span-8 flex justify-center">
-        <div className="flex border border-gray-500 rounded-2xl overflow-hidden w-full max-w-md">
-          <input
-            type="text"
-            className="px-3 py-1 outline-none w-full"
-            placeholder="Search"
-          />
-          <button className="bg-gray-200 px-3">
-            <IoSearch />
-          </button>
+        <div className="relative w-full max-w-md">
+          <div className="flex border border-gray-500 rounded-2xl overflow-hidden w-full max-w-md">
+            <input
+              type="text"
+              className="px-3 py-1 outline-none w-full"
+              placeholder="Search"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button className="bg-gray-200 px-3">
+              <IoSearch />
+            </button>
+          </div>
+
+          {suggestions.length > 0 && (
+            <div className="absolute top-full  left-0 w-full bg-white  border py-2 px-4 border-gray-300 rounded-md mt-1 shadow-lg z-50">
+              <ul>
+                {suggestions.map((s) => (
+                  <li
+                    key={s}
+                    className="flex items-center gap-2 hover:bg-gray-100 px-2 py-1 rounded"
+                  >
+                    <IoSearch className="text-gray-500" />
+                    {s}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       </div>
       <div className="col-span-2 flex justify-end">
