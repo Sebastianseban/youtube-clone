8b712f67f2063e29a0898e0415f814b935b48080
sebastian <sebastianseban0105@gmail.com>
2025-06-06 08:17:06 +0530

    feat: added message feature in LiveChat

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -1,91 +1,80 @@
-// import React from 'react'
-// import ChatMessage from './ChatMessage'
-
-// const LiveChat = () => {
-//   return (
-//     <div className='w-[400px] h-full border rounded-lg '>
-//         <div className='border-b px-3 py-1'>
-//             <h1>Top Chat</h1>
-//         </div>
-//         <div>
-//             <ChatMessage/>
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default LiveChat
-
-import React from 'react';
-import ChatMessage from './ChatMessage';
+import React, { useEffect, useState } from "react";
+import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../store/chatSlice";
 
 export const DUMMY_MESSAGES = [
   {
     id: 1,
-    name: "CodeMaster77",
-    message: "Let's gooo! This stream is 🔥",
+    name: "CodeWizard",
+    message: "Finally fixed that async bug. Took me 3 hours 😅",
   },
   {
     id: 2,
-    name: "PixelQueen",
-    message: "Can someone share the GitHub repo?",
+    name: "TechSavvy",
+    message: "Dark mode supremacy 💻🌙",
   },
-  {
-    id: 3,
-    name: "DevDude",
-    message: "Big fan from Kerala ❤️",
-  },
-  {
-    id: 4,
-    name: "AI_Bro",
-    message: "React is love, React is life 😎",
-  },
-  {
-    id: 5,
-    name: "TechieSara",
-    message: "This UI looks just like YouTube, great job!",
-  },
-  {
-    id: 6,
-    name: "Seban",
-    message: "Seban and Alaska, forever.",
-  },
-  {
-    id: 7,
-    name: "BugHunter",
-    message: "I broke the code and fixed it again 😂",
-  },
-  {
-    id: 8,
-    name: "ChillVibes",
-    message: "Streaming + coding = perfect combo",
-  },
+  // ... rest unchanged
 ];
 
-
 const LiveChat = () => {
+  const dispatch = useDispatch();
+  const [chatMsg, setChatMsg] = useState("");
+  const chatMessage = useSelector((store) => store.chat.messages);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * DUMMY_MESSAGES.length);
+      const message = DUMMY_MESSAGES[randomIndex];
+
+      dispatch(
+        addMessage({
+          ...message,
+          id: Date.now() + Math.random(), // unique id
+        })
+      );
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="w-[400px] h-full bg-white dark:bg-gray-900 border border-gray-300 dark:border-gray-700 rounded-lg flex flex-col">
-      
-
+    <div className="w-[400px] h-[600px] bg-white dark:bg-gray-900 border border-gray-300 dark:border-gray-700 rounded-lg flex flex-col">
       <div className="border-b border-gray-300 dark:border-gray-700 px-4 py-2 bg-gray-100 dark:bg-gray-800">
-        <h1 className="text-sm font-semibold text-gray-800 dark:text-white">Top Chat</h1>
-      </div>
-      
-
-      <div className="flex-1 overflow-y-auto p-4 space-y-4">
-        {DUMMY_MESSAGES.map((msg)=><ChatMessage key={msg.id} name={msg.name} message={msg.message} />)}
-       
+        <h1 className="text-sm font-semibold text-gray-800 dark:text-white">
+          Top Chat
+        </h1>
       </div>
 
+      <div className="flex-1 overflow-y-auto p-4 flex flex-col-reverse space-y-4 space-y-reverse">
+        {chatMessage.map((msg) => (
+          <ChatMessage key={msg.id} name={msg.name} message={msg.message} />
+        ))}
+      </div>
 
-      <div className="border-t border-gray-300 dark:border-gray-700 p-3">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (chatMsg.trim() === "") return;
+          dispatch(
+            addMessage({
+              id: Date.now(), // unique id for user message
+              name: "seban",
+              message: chatMsg.trim(),
+            })
+          );
+          setChatMsg("");
+        }}
+        className="border-t border-gray-300 dark:border-gray-700 p-3"
+      >
         <input
+          onChange={(e) => setChatMsg(e.target.value)}
+          value={chatMsg}
           type="text"
           placeholder="Chat..."
           className="w-full px-4 py-2 text-sm border border-gray-300 dark:border-gray-600 rounded-full bg-white dark:bg-gray-800 text-gray-900 dark:text-white focus:outline-none"
         />
-      </div>
+      </form>
     </div>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,5 @@ export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/vide
 export const VIDEO_DETAILS_API = (videoId) => `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=${videoId}&key=${import.meta.env.VITE_YOUTUBE_API_KEY}`;
 export const YOUTUBE_SEARCH_API =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const LIVE_CHAT_COUNT =10
